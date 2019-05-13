093064e9b8dffcf0fbe4eb69b0151668598fed5d
George Dewhirst <dewhirst.george@gmail.com>
2019-05-13 21:53:24 +1000

    second data upload and final prediction handler

--- prediction_handler.py
+++ prediction_handler.py
@@ -3,6 +3,7 @@ import pandas as pd
 import tensorflow as tf
 from tensorflow import keras
 from tensorflow.keras import layers
+from tensorflow.keras import regularizers
 
 class PredictionNN:
     
@@ -10,20 +11,21 @@ class PredictionNN:
         self.embedding_handler = embedding_handler
         num_heroes = len(embedding_handler.heroes_df['id'])
         embedding_weights = embedding_handler.compiled_network.layers[2].get_weights()
-        #this is just a basic placeholder network
+        #this network seemed to perform the best after 3 epochs with ~730k training examples
         #input layer
         inputs = tf.keras.Input(shape=(10,))
         #embedding layer
-        embedding = layers.Embedding(num_heroes,embedding_handler.embedding_dim,input_length=10,weights=embedding_weights,trainable=False)(inputs)
+        embedding = layers.Embedding(num_heroes,15,input_length=10,trainable=True)(inputs)
         flattener = layers.Flatten()(embedding)
         #hidden layer - not finalised still tweaking parameters
-        hidden1 = layers.Dense(80, activation='sigmoid')(flattener)
-        hidden2 = layers.Dense(80, activation='sigmoid')(hidden1)
-        hidden3 = layers.Dense(80, activation='sigmoid')(hidden2)
-        hidden4 = layers.Dense(80, activation='sigmoid')(hidden3)
-        hidden5 = layers.Dense(80, activation='sigmoid')(hidden4)
+        hidden1 = layers.Dense(150,activation='relu')(flattener)
+        dropout1 = layers.Dropout(0.2)(hidden1)
+        hidden2 = layers.Dense(150,activation='relu')(dropout1)
+        dropout2 = layers.Dropout(0.2)(hidden2)
+        hidden3 = layers.Dense(75,activation='relu')(dropout2)
+        hidden4 = layers.Dense(40,activation='relu')(hidden3)
         #output layer
-        output = layers.Dense(1, activation='sigmoid')(hidden5)
+        output = layers.Dense(1, activation='sigmoid')(hidden4)
         #combined network
         predictor_network = tf.keras.Model(inputs=inputs,outputs=output)
         predictor_network.compile(optimizer='rmsprop', loss='binary_crossentropy', metrics=['accuracy'])
